1239480e7291fd1804a35abe72f8ff148669510c
Selena Nguyen <snguyen3320@hwdsb.on.ca>
2026-02-28 10:46:10 -0500

    initialize

---
+++ penguin.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+var speed = 200
+
+var motion = Vector2.ZERO
+
+func _physics_process(delta):
+	motion = move_and_collide(Vector2(0, speed * delta))
+	
+	if Input.is_action_pressed("left"):
+		motion = move_and_collide(Vector2(-speed * delta, 0))
+		$Sprite.flip_h = true
+	if Input.is_action_pressed("right"):
+		motion = move_and_collide(Vector2(speed * delta, 0))
+		$Sprite.flip_h = false
+	
