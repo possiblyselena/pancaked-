f2de0352d872876eda3dd6e3ea082bc50ae4d3f9
Selena <149027636+possiblyselena@users.noreply.github.com>
2026-02-28 16:22:24 -0500

    collision box

--- apple.gd
+++ apple.gd
@@ -1,5 +1,11 @@
 extends Sprite2D
 
+func _ready() -> void:
+	$applecollision.area_entered.connect(onAreaEntered)
 
 func _process(delta: float) -> void:
 	position.y += 500 * delta
+
+func onAreaEntered(area:Area2D) -> void:
+	print(area)
+ 
